fb3a27994aa1628960aa34c21b24b6216629ac39
Yuriy <Seaman_on_watch@mail.ru>
2019-10-06 21:46:20 +0300

    Specflow Scenarios

---
+++ SpecflowProject/StepDefinitions/SpecFlowFeature2Steps.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace SpecflowProject.StepDefinitions
+{
+    [Binding]
+    public class SpecFlowFeature2Steps
+    {
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredIntoTheCalculator(int numbers)
+        {
+            Console.WriteLine(numbers);
+        }
+        
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            Console.WriteLine("Pressed Add button");
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int result)
+        {
+            if (result == 121)
+            {
+                Console.WriteLine("The test PASS");
+            }
+            else
+            {
+                Console.WriteLine("The test FAIL");
+                throw new Exception("The value is different");
+            }
+        }
+    }
+}
